1c0e7aa3154d5ff6483c073dffa81d53d471b370
akcs47 <49140375+akcs47@users.noreply.github.com>
2019-04-08 18:56:06 +0530

    Create euler18.cpp

---
+++ euler18.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    int a[n][n] = { { 0 } };
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            cin >> a[i][j];
+        }
+    }
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            if(i == 0) 
+                a[i][j]  += a[i-1][j];
+            else if(j == i)
+                a[i][j]  += a[i-1][j-1];
+            else 
+                a[i][j] = max(a[i][j]+a[i-1][j-1], a[i][j]+a[i-1][j]);
+        }
+    }
+    int max = INT_MIN;
+    for(int i = 0; i < n; i++){
+        if(a[n-1][i] > max) max = a[n-1][i]; 
+    }
+    cout << max;
+    return 0;
+}
